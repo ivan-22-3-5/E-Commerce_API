f813697da1713111b03007c9442e919e64ce7ab5
Ivan Mihun <vsk76090@gmail.com>
2024-06-10 20:36:42 +0200

    added address schemas and crud

---
+++ src/schemas/address.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+from src.schemas.base import ObjUpdate
+
+
+class AddressIn(BaseModel):
+    fullname: str
+    country: str
+    city: str
+    street: str
+    zipcode: int
+
+
+class AddressUpdate(ObjUpdate):
+    fullname: Optional[str] = Field(default=None)
+    country: Optional[str] = Field(default=None)
+    city: Optional[str] = Field(default=None)
+    street: Optional[str] = Field(default=None)
+    zipcode: Optional[int] = Field(default=None)
