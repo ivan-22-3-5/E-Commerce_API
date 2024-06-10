6af7f0e59f12636f2009c279e942b72a3348aa50
Ivan Mihun <vsk76090@gmail.com>
2024-06-10 20:27:37 +0200

    added Address, Category, Review, Cart and CartItem models. + minor changes

--- src/db/models.py
+++ src/db/models.py
@@ -1,12 +1,19 @@
 from datetime import datetime
 
-from sqlalchemy import Integer, String, TIMESTAMP, ForeignKey, Float, Boolean, Enum
+from sqlalchemy import Integer, String, TIMESTAMP, ForeignKey, Float, Boolean, Enum, Table, Column, PrimaryKeyConstraint
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 
 from src.custom_types import OrderStatus
 from src.db.db import Base
+
+product_category_association = Table(
+    'product_category_association', Base.metadata,
+    Column('product_id', Integer, ForeignKey('products.id'), primary_key=True),
+    Column('category_name', String, ForeignKey('categories.name'), primary_key=True),
+    PrimaryKeyConstraint('product_id', 'category_name')
+)
 
 
 class User(Base):
@@ -18,8 +25,11 @@ class User(Base):
     created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
     is_admin: Mapped[bool] = mapped_column(Boolean, nullable=False)
 
-    refresh_token = relationship('RefreshToken', back_populates='user')
+    refresh_token = relationship('RefreshToken', back_populates='user', uselist=False)
     orders = relationship('Order', back_populates='user')
+    reviews = relationship('Review', back_populates='user')
+    addresses = relationship('Address', back_populates='user')
+    cart = relationship('Cart', uselist=False)
 
     def __init__(self, email: str, password: str, username: str):
         self.email = email
@@ -29,13 +39,47 @@ class User(Base):
         self.is_admin = False
 
 
+class Address(Base):
+    __tablename__ = 'addresses'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
+    fullname: Mapped[str] = mapped_column(String, nullable=False)
+    country: Mapped[str] = mapped_column(String, nullable=False)
+    city: Mapped[str] = mapped_column(String, nullable=False)
+    street: Mapped[str] = mapped_column(String, nullable=False)
+    zipcode: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    user = relationship('User', back_populates='addresses', uselist=False)
+
+
 class RefreshToken(Base):
     __tablename__ = 'refresh_tokens'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), unique=True, nullable=False)
     token: Mapped[str] = mapped_column(String, nullable=False)
 
-    user = relationship('User', back_populates='refresh_token')
+    user = relationship('User', back_populates='refresh_token', uselist=False)
+
+
+class Cart(Base):
+    __tablename__ = 'carts'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), unique=True, nullable=False)
+
+    items = relationship('CartItem')
+
+
+class CartItem(Base):
+    __tablename__ = 'cart_items'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    product_id: Mapped[int] = mapped_column(Integer, ForeignKey('products.id'), nullable=False)
+    cart_id: Mapped[int] = mapped_column(Integer, ForeignKey('carts.id'), nullable=False)
+    quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    def __init__(self, product_id: int, cart_id: int):
+        self.product_id = product_id
+        self.cart_id = cart_id
+        self.quantity = 0
 
 
 class Order(Base):
@@ -45,9 +89,11 @@ class Order(Base):
     status: Mapped[OrderStatus] = mapped_column(Enum(OrderStatus), nullable=False)
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
     product_id: Mapped[int] = mapped_column(Integer, ForeignKey('products.id'))
+    address_id: Mapped[int] = mapped_column(Integer, ForeignKey('addresses.id'))
 
-    user = relationship('User', back_populates='orders')
-    product = relationship('Product', back_populates='orders')
+    user = relationship('User', back_populates='orders', uselist=False)
+    product = relationship('Product', back_populates='orders', uselist=False)
+    address = relationship('Address', uselist=False)
 
     def __init__(self, user_id: int, product_id: int):
         self.created_at = datetime.now()
@@ -65,7 +111,9 @@ class Product(Base):
     created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
     enabled: Mapped[bool] = mapped_column(Boolean, nullable=False)
 
-    orders = relationship('Order', back_populates='product', cascade="all, delete")
+    orders = relationship('Order', back_populates='product', cascade='all, delete')
+    categories = relationship('Category', back_populates='products', secondary=product_category_association)
+    reviews = relationship('Review', back_populates='product')
 
     def __init__(self, title: str, description: str, price: float):
         self.title = title
@@ -73,3 +121,35 @@ class Product(Base):
         self.price = price
         self.created_at = datetime.now()
         self.enabled = True
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+    name: Mapped[str] = mapped_column(String, primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
+
+    products = relationship('Product', back_populates='categories', secondary=product_category_association)
+
+    def __init__(self, name: str):
+        self.name = name
+        self.created_at = datetime.now()
+
+
+class Review(Base):
+    __tablename__ = 'reviews'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
+    product_id: Mapped[int] = mapped_column(Integer, ForeignKey('products.id'))
+    rating: Mapped[int] = mapped_column(Integer, nullable=False)
+    content: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
+
+    product = relationship('Product', back_populates='reviews', uselist=False)
+    user = relationship('User', back_populates='reviews', uselist=False)
+
+    def __init__(self, user_id: int, product_id: int, rating: int, content: str):
+        self.user_id = user_id
+        self.product_id = product_id
+        self.rating = rating
+        self.content = content
+        self.created_at = datetime.now()
