4b3dec67d36c1d02a0b4c79fab4df81f424caab2
Ivan Mihun <vsk76090@gmail.com>
2024-07-27 19:43:06 +0200

    add PaymentIntent creation

--- src/routers/orders.py
+++ src/routers/orders.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter, status
+import stripe
+from fastapi import APIRouter, status, Request
 
+from src.config import settings
 from src.constraints import admin_path, confirmed_email_required
 from src.crud import orders, products, addresses
 from src.custom_types import OrderStatus
+from src.schemas.client_secret import ClientSecret
 from src.schemas.message import Message
-from src.schemas.order import OrderIn, OrderOut
+from src.schemas.order import OrderIn
 from src.deps import cur_user_dependency, db_dependency
 from src.custom_exceptions import ResourceDoesNotExistError, NotEnoughRightsError
 
@@ -15,7 +18,7 @@ router = APIRouter(
 
 
 @confirmed_email_required
-@router.post('', status_code=status.HTTP_201_CREATED, response_model=OrderOut)
+@router.post('', status_code=status.HTTP_201_CREATED, response_model=ClientSecret)
 async def create_order(user: cur_user_dependency, order: OrderIn, db: db_dependency):
     if (address := await addresses.get_by_id(order.address_id, db)) is None:
         raise ResourceDoesNotExistError("Address with the given id does not exist")
@@ -24,7 +27,19 @@ async def create_order(user: cur_user_dependency, order: OrderIn, db: db_depende
     for item in order.items:
         if not await products.get_by_id(item.product_id, db):
             raise ResourceDoesNotExistError("Product with the given id does not exist")
-    return await orders.create(order, user.id, db=db)
+    order = await orders.create(order, user.id, db=db)
+    payment_intent = stripe.PaymentIntent.create(
+        amount=int(order.total_price * 100),
+        currency="usd",
+        automatic_payment_methods={
+            "enabled": True
+        },
+        metadata={
+            "order_id": str(order.id)
+        },
+        api_key=settings.STRIPE_SECRET_KEY
+    )
+    return ClientSecret(client_secret=payment_intent.client_secret)
 
 
 @router.post('/{order_id}/cancel', status_code=status.HTTP_200_OK, response_model=Message)
